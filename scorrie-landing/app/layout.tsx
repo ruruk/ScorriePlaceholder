d303c9ae159247b3cef325fa3aefd9a98b736ea7
Ruan-Klopper <231280@virtualwindow.co.za>
2025-03-24 22:02:36 +0200

    Update layout.tsx

--- scorrie-landing/app/layout.tsx
+++ scorrie-landing/app/layout.tsx
@@ -9,7 +9,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Scorrie | A Revolutionary Trust-First Marketplace",
+  title: "Scorrie",
   description:
     "Scorrie is building a revolutionary platform where trust meets transactions. Connect safely in a world full of uncertainty. Launching June 2026.",
   keywords:
